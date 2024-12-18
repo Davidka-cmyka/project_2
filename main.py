c3d87e62ebdf684097aed3984bbf72671fe253d6
Davidka-cmyka <daleksian@gmail.com>
2024-12-18 13:39:36 +0300

    задал структуру

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pandas as pd
 from openpyxl import load_workbook
 from functions import update_ids
 
-# Пути к файлам
-file_paths = ['file1.xlsx', 'file2.xlsx', 'file3.xlsx']
+def main():
+    # Пути к файлам
+    file_paths = ['file1.xlsx', 'file2.xlsx', 'file3.xlsx']
 
-# Вызов функции
-update_ids(file_paths)
+    # Вызов функции
+    update_ids(file_paths)
 
-print("Обновление ID завершено во всех файлах.")
+    print("Обновление ID завершено во всех файлах.")
+
+if __name__  == "__main__":
+    main()
+
